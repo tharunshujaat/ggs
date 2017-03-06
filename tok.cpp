c50b2720746373f085c681239ebf25b3abde30f4
tharunshujaat <tharunshujaat5@gmail.com>
2017-03-06 14:42:55 +0530

    Add files via upload

---
+++ tok.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a=2;
+  double const b=4;
+  float c=1.5;
+  char d='A';
+  cout<<"the value of a is"<<a<<"\n";
+  cout<<"the value of b is"<<b<<"\n";
+  cout<<"the value of c is"<<c<<"\n";
+  cout<<"the value of d is"<<d<<"\n";
+  return 0;
+}
